a3fca59b77056e69081a378ae9dfc7a4e757cb19
Zaw Waiyan Lwin <zawwaiyanlwin1996@gmail.com>
2015-11-11 16:19:43 +0800

    User Class
    Class model for user

---
+++ private/models/user.php
@@ -0,0 +1,25 @@
+<?php
+namespace private\models;
+	
+class user	{
+	private $connection	=	NULL;
+
+	public function __construct( $dbConn )	{
+		$connection	=	$dbConn;
+	}
+
+	public function login()	{
+
+	}
+
+	public function logout()	{
+
+	}
+
+	public function register()	{
+
+	}
+
+	public function 
+}
+?>
