69d62d5d7e63d4bd211e8e1621ac8a1c6580ad9b
Zaw Waiyan Lwin <zawwaiyanlwin1996@gmail.com>
2015-11-11 17:20:04 +0800

    Login controller and sample
    Included login controller to index.php and sample of how it should
    function.

--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
 <?php
+include '../vendor/autoload.php';
+include '../private/controllers/login.php';
 
+$page	=	NULL;
+$templatePath	=	'../private/templates/';
+
+if ( isset($_POST['login']) )	{
+	
+}
+
+$page	=	include $templatePath . "header.php";
+$page  .=	include $templatePath . "navigation.php";
+$page  .=	include $templatePath . "login.php";
+$page  .=	include $templatePath . "footer.php";
+
+echo $page;
 ?>
